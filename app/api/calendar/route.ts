4b64076a500870a7422b1033be55a9a1c42a2a0c
Arindam <arindamdawn3@gmail.com>
2023-12-06 16:41:40 +0530

    fix: update calendar api to fix error

--- app/api/calendar/route.ts
+++ app/api/calendar/route.ts
@@ -5,17 +5,11 @@ import {
 } from './gaurabda-calendar/index.js';
 
 export async function GET() {
-  const tc = new TResultCalendar();
-  const location = GCLocation.FindByName('Calcutta');
-  const startDate = new GregorianDateTime();
-  tc.CalculateCalendar(location, startDate, 30);
-  const events = tc.m_pData
-    .filter((d, index) => index > tc.BEFORE_DAYS)
-    .map((d, index) => {
-      if (index <= tc.BEFORE_DAYS)
-        return {
-          event: d['dayEvents'],
-        };
-    });
-  return Response.json({ data: events, length: events.length });
+  try {
+    // const tc = new TResultCalendar();
+    // const location = GCLocation.FindByName('Calcutta');
+    // const startDate = new GregorianDateTime();
+  } catch (error) {}
+
+  return Response.json({ data: '' });
 }
